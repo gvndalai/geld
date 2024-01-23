a2601d3eb8e9984c1914b993657a2bb25dddeb27
gvndalai <gundalai254@gmail.com>
2024-01-23 19:03:12 +0800

    ygaad

---
+++ src/pages/login.js
@@ -0,0 +1,11 @@
+import React from "react";
+const Login = () => {
+  <>
+    <div className="w-screen flex">
+      <div className="w-full bg-white">sdasdsadasd</div>
+      <div className="bg-blue-500 h-screen w-full"></div>
+    </div>
+  </>;
+};
+
+export default Login;
